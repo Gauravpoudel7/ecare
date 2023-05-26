7929bc1f10e925204a2d007b5ad5570f1b321de7
Gaurav <gauravpoudel1068@gmail.com>
2023-05-26 18:24:05 +0545

    views added home,patients & templates button,urls added

--- ecare/core/views.py
+++ ecare/core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+def home(request):
+    if request.method == "POST":
+        username = request.POST.get("usernamep")
+        password = request.POST.get("passp")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("patient")
+        else:
+            return redirect("home")
+    else:
+        return render(request, "home.html")
+
+
+def patient(request):
+    return render(request, "patient.html")
